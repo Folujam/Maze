0468f97aa9777bf051b1bfe86aa33b4e364cf2b0
folujam <olusway@gmail.com>
2024-06-08 15:48:41 +0100

    maze game

--- src/create_instance.c
+++ src/create_instance.c
@@ -14,7 +14,7 @@ int init_instance(SDL_Instance *instance)
 		return (1);
 	}
 	/*creates a new window instance*/
-	instance->window = SDL_CreateWindow("SDL2 \\o/", SDL_WINDOWPOS_CENTERED,\
+	instance->window = SDL_CreateWindow("Maze Game", SDL_WINDOWPOS_CENTERED,\
 		SDL_WINDOWPOS_CENTERED, 1260, 720, 0);
 	if (instance->window == NULL)
 	{
