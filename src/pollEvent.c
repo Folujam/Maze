d22078de788ee6032aa174fdf481ae72ee62494b
Folujam <olusway@gmail.com>
2025-08-26 21:28:01 +0100

    implement worldmap, walls, and player movement

--- src/pollEvent.c
+++ src/pollEvent.c
@@ -1,4 +1,4 @@
-#include "../dependacies/header.h"
+#include "../dependencies/header.h"
 
 /**
 * poll_events - loops tru to finds what event occured and executes it
@@ -20,6 +20,47 @@ int poll_events(void)
 				/*if esc pressed*/
 				if (key.keysym.scancode == 0x29)
 					return (1);
+
+				// Forward (W)
+                if (key.keysym.scancode == SDL_SCANCODE_W)
+                {
+                    if (worldMap[(int)(posX + dirX * moveSpeed)][(int)posY] == 0)
+                        posX += dirX * moveSpeed;
+                    if (worldMap[(int)posX][(int)(posY + dirY * moveSpeed)] == 0)
+                        posY += dirY * moveSpeed;
+                }
+
+                // Backward (S)
+                if (key.keysym.scancode == SDL_SCANCODE_S)
+                {
+                    if (worldMap[(int)(posX - dirX * moveSpeed)][(int)posY] == 0)
+                        posX -= dirX * moveSpeed;
+                    if (worldMap[(int)posX][(int)(posY - dirY * moveSpeed)] == 0)
+                        posY -= dirY * moveSpeed;
+                }
+
+                // Rotate Left (A)
+                if (key.keysym.scancode == SDL_SCANCODE_A)
+                {
+                    double oldDirX = dirX;
+                    dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
+                    dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
+                    double oldPlaneX = planeX;
+                    planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
+                    planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
+                }
+
+                // Rotate Right (D)
+                if (key.keysym.scancode == SDL_SCANCODE_D)
+                {
+                    double oldDirX = dirX;
+                    dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
+                    dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
+                    double oldPlaneX = planeX;
+                    planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
+                    planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
+                }
+
 				break;
 		}
 	}
